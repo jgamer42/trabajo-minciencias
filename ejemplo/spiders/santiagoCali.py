b8c68a169b34b06ac84a8dccc3ad8977b5b7d5b1
jaime <jgaimer42@gmail.com>
2021-01-07 16:33:11 -0500

    dos universidades listas

---
+++ ejemplo/spiders/santiagoCali.py
@@ -0,0 +1,42 @@
+import scrapy
+from ..items import EjemploItem
+import sys
+
+class santiagoCali(scrapy.Spider):
+    name = 'santiagoCali'
+    start_urls = ["http://utopicos.com.co/index.php/component/search/?searchword=conflicto%20armado&searchphrase=all&limitstart=0"]
+    page = 20
+
+    def parse(self,response):
+        links = response.xpath("//dt/a/@href").getall()
+        for link in links:
+            link = "http://utopicos.com.co"+link
+            yield response.follow(url=link,callback=self.get_info,cb_kwargs={"link":link})
+            
+        next_page = f"http://utopicos.com.co/index.php/component/search/?searchword=conflicto%20armado&searchphrase=all&limitstart={self.page}"
+        if self.page <= 40:
+            self.page = self.page + 20
+            yield response.follow(url=next_page,callback=self.parse)
+
+
+
+    def get_info(self,response,**kwargs):
+        item = EjemploItem()
+        title = response.xpath("//h1/a/text()").get()
+        title = title.strip()
+        date = response.xpath("//time/text()").getall()[0]
+        content = response.xpath("//section/p/text()").getall()
+        aux = response.xpath("//section/p/strong/text() | //section/p/strong/em/text()").getall()
+        content = " ".join(content)
+        aux = "".join(aux)
+        print(f"\n\n\n\n\n\n\n {content}")
+        title = title.strip()
+        date = date.strip()
+        item["link"]=kwargs["link"]
+        item["numero"]=1
+        item["titulo"] = title
+        item["fecha"] = date
+        item["contenido"] = content
+        item["contenido_auxiliar"] = aux
+        item["medio"]=self.name
+        yield item
