568e487d7fc489a38fa332a41a1454f202b1f16d
jaime <jgaimer42@gmail.com>
2021-01-27 14:54:21 -0500

    unficada la busqueda de eafit, primer acercamiento al analisis de fechas

---
+++ tools/informe_fechas.py
@@ -0,0 +1,19 @@
+import json
+import site
+from datetime import datetime
+site.addsitedir("/home/jaime/compartida/codigo/trabajo-minciencias/utils")
+import control_fechas 
+#file = open("../exports/tadeo.json")
+#file = open("../exports/eafit.json")
+raw_data = json.load(file)
+fechas = []
+for data in raw_data:
+    if data["fecha"] != None:
+        fecha = control_fechas.valor(data["fecha"].strip())
+        if fecha != 0:
+            fecha = f"{fecha[0]}/{fecha[1]}/{fecha[2]}"
+            fechas.append(fecha)
+ordenadas = fechas.sort(key= lambda date:datetime.strptime(date,'%d/%m/%Y'))
+print(fechas)
+
+
