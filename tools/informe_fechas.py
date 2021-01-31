65aa0ce2a007bbc4ffc0e155589109eb68105293
jaime <jgaimer42@gmail.com>
2021-01-30 21:59:46 -0500

    terminando el sistema de informes por fechas , agregado script para ejecutar todos los spiders

--- tools/informe_fechas.py
+++ tools/informe_fechas.py
@@ -2,20 +2,45 @@ import json
 import site
 from datetime import datetime
 site.addsitedir("/home/jaime/compartida/codigo/trabajo-minciencias/utils")
-import control_fechas 
-#file = open("../exports/tadeo.json")
-#file = open("../exports/eafit.json")
-file = open("../exports/uniminutoradio.json")
-raw_data = json.load(file)
-fechas = []
-for data in raw_data:
-    print(data["fecha"])
-    if data["fecha"] != None:
-        fecha = control_fechas.valor(data["fecha"].strip())
-        if fecha != 0:
-            fecha = f"{fecha[0]}/{fecha[1]}/{fecha[2]}"
-            fechas.append(fecha)
-ordenadas = fechas.sort(key= lambda date:datetime.strptime(date,'%d/%m/%Y'))
-print(fechas)
+import control_fechas
+import configparser
+config = configparser.ConfigParser()
+config.sections()
+config.read("/home/jaime/compartida/codigo/trabajo-minciencias/general.cfg")
+carpetas = config["carpetas"]["carpetas"]
+carpetas = carpetas.split(",") 
+print(carpetas)
+for carpeta in carpetas:
+    print(f"\n\n{carpeta}")
+    file = open(f"../exports/{carpeta}.json")
+    raw_data = json.load(file)
+    fechas = []
+    if "no dis" in raw_data[0]["fecha"]:
+        print("no disponible")
+        file.close()
+    else:
+        for data in raw_data:
+            if data["medio"] == "bolivarianaMedellin":
+                fecha = data["fecha"]
+                if "-" in fecha:
+                    pass
+                elif "/" in fecha:
+                    fecha = list(map(int,fecha.split("/")))
+                    fecha = f"{fecha[0]}/{fecha[1]}/{fecha[2]}"
+                    fechas.append(fecha)
+            elif data["fecha"] != None:
+                fecha = control_fechas.valor(data["fecha"].strip())
+                if fecha != 0:
+                    fecha = f"{fecha[0]}/{fecha[1]}/{fecha[2]}"
+                    fechas.append(fecha)
+        fechas.sort(key = lambda date:datetime.strptime(date,'%d/%m/%Y'))
+        print(fechas[0],"-",fechas[1])
+        file.close()
+
+
+    
+       
+
+
 
 
